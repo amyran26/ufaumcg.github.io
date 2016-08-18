f05687b960066e8b5e16bedb5f7af00c3334bd3f
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-08-18 10:50:18 -0700

    refactor css, made navbar more graceful

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Website for the Undergraduate Students Association by Innovative Design.
+Website for the Undergraduate Students Association by Innovative Design. Currently hosted at <https://github.com/bsmarimon/UFA>. 
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
   $(".menu").click(function() {
-    $(".menu").addClass("responsive-menu");
-    $(".navbar-item").addClass("navbar-item-responsive");
-    $(".navbar").addClass("navbar-responsive");
-    $(".landing-container").addClass("landing-container-responsive");
-    $(".body-container").hide();
+    if ($(".menu")[0].innerHTML.indexOf("MENU") > -1) {
+      $(".menu").addClass("responsive-menu");
+      $(".navbar-item").addClass("navbar-item-responsive");
+      $(".navbar").addClass("navbar-responsive");
+      $(".landing-container").addClass("landing-container-responsive");
+      $(".body-container").hide();
+      $(".landing-container").hide();
+      $(".menu")[0].innerHTML = "CLOSE";
+    } else {
+      $(".menu").removeClass("responsive-menu");
+      $(".navbar-item").removeClass("navbar-item-responsive");
+      $(".navbar").removeClass("navbar-responsive");
+      $(".landing-container").removeClass("landing-container-responsive");
+      $(".body-container").show();
+      $(".landing-container").show();
+      $(".menu")[0].innerHTML = "MENU";
+    }
   });
 });
