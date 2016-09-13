703d2fc8bf4c58bbe7a6b33296977566f7fe4e05
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-09-13 16:28:59 -0700

    Fix page contents disappearing on navbar click

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
   $(".navbar").click(function() {
-    if ($(".menu")[0].innerHTML.indexOf("MENU") > -1) {
-      $(".menu").addClass("responsive-menu");
-      $(".navbar-item").addClass("navbar-item-responsive");
-      $(".navbar").addClass("navbar-responsive");
-      $(".landing-container").addClass("landing-container-responsive");
-      // Hide whatever the container is on the page
-      $(".body-container").hide();
-      $(".landing-container").hide();
-      $(".menu")[0].innerHTML = "CLOSE";
-    } else {
-      $(".menu").removeClass("responsive-menu");
-      $(".navbar-item").removeClass("navbar-item-responsive");
-      $(".navbar").removeClass("navbar-responsive");
-      $(".landing-container").removeClass("landing-container-responsive");
-      // Show whatever the container is on the page
-      $(".body-container").show();
-      $(".landing-container").show();
-      $(".menu")[0].innerHTML = "MENU";
+    if ($(window).width() < 775) {
+      if ($(".menu")[0].innerHTML.indexOf("MENU") > -1) {
+        $(".menu").addClass("responsive-menu");
+        $(".navbar-item").addClass("navbar-item-responsive");
+        $(".navbar").addClass("navbar-responsive");
+        $(".landing-container").addClass("landing-container-responsive");
+        // Hide whatever the container is on the page
+        $(".body-container").hide();
+        $(".landing-container").hide();
+        $(".menu")[0].innerHTML = "CLOSE";
+      } else {
+        $(".menu").removeClass("responsive-menu");
+        $(".navbar-item").removeClass("navbar-item-responsive");
+        $(".navbar").removeClass("navbar-responsive");
+        $(".landing-container").removeClass("landing-container-responsive");
+        // Show whatever the container is on the page
+        $(".body-container").show();
+        $(".landing-container").show();
+        $(".menu")[0].innerHTML = "MENU";
+      }
     }
   });
 });
